e5432a5eaa227f84f7c31a4242a58e3a41b6e36f
Naran Torguud <ntorguud@gmail.com>
2021-04-12 18:47:20 +0000

    Create first JS page

---
+++ calculator.js
@@ -0,0 +1,7 @@
+console.log("Welcome to my calculator");
+
+var x = 5;         
+var y = 7;         
+var z = x + y; 
+// console.log("The sum of", x, '+', y, "is:", z);
+console.log(`The sum of ${x} + ${y} is: ${z}`);
