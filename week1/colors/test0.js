7ac777997dcee74ab258db99465f17f42890eee2
Naran Torguud <ntorguud@gmail.com>
2021-04-21 18:49:52 +0000

    Add contents

--- week1/colors/test0.js
+++ week1/colors/test0.js
@@ -11,6 +11,22 @@ console.log('"Lie ' + 'on ' + 'the ' + 'ocean"');
 "con" + "cat" + "e" + "nate"
 // this is a Backtick quoted strings, usually called template literals `half of 100 is ${100 / 2}`
 typeof 4.5
-typeof 
+typeof "x"
+3 > 2
+3 < 2 
+"Aardvark" < "Zoroaster"
+"Itchy" != "Scratchy"
+"Apple" == "Orange"
+//surprisingly this is going to result in  false
+NaN == NaN 
+true && false
+true && true
+false || true
+false || false
+!true
+1 + 1 == 2 && 10 * 10 > 50
+//ternary operator
+true ? 1 : 2
+false ? 1 : 2
 
 
