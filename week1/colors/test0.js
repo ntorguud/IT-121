5a67a4f6feb9957787452ce3db3c257ecb916353
Naran Torguud <ntorguud@gmail.com>
2021-04-21 05:42:25 +0000

    Changes

---
+++ week1/colors/test0.js
@@ -0,0 +1,16 @@
+console.log(100 + 4 * 11);
+console.log((100 + 4) * 11);
+
+console.log('Down on the sea');
+console.log('"Lie on the ocean"');
+console.log('"Lie ' + 'on ' + 'the ' + 'ocean"'); 
+
+
+"This is the first line\nAnd this is the second"
+"A new line character is written like \"\\n\"."
+"con" + "cat" + "e" + "nate"
+// this is a Backtick quoted strings, usually called template literals `half of 100 is ${100 / 2}`
+typeof 4.5
+typeof 
+
+
