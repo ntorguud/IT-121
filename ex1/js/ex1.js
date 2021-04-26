6062e206c71f3266103fb1e8e6882b9d62fbe61c
Naran Torguud <ntorguud@gmail.com>
2021-04-26 19:43:30 +0000

    Change div id name

--- ex1/js/ex1.js
+++ ex1/js/ex1.js
@@ -3,5 +3,5 @@ var y = 7;
 
 var total = x + y;
 
-var el = document.getElementById('cost');
+var el = document.getElementById('message');
 el.textContent = 'The sum of ' +  x + ' + ' + y  + ' is : ' + total;
