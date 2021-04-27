3798e5ede53e0a75ed09dc520e892626053f2b3b
Naran Torguud <ntorguud@gmail.com>
2021-04-27 19:07:37 +0000

    Add contents

--- week3/ex2/js/object-literal.js
+++ week3/ex2/js/object-literal.js
@@ -1,20 +1,22 @@
 // Set up the object
 var hotel = {
-    name : 'Quay',
-    rooms : 40,
-    booked : 25,
- 
-    checkAvailability : function() {
-      return this.rooms - this.booked; // Need "this" (refers keyword) because inside function
+    name: 'Quay',
+    rooms: 40,
+    booked: 25,
+
+    checkAvailability: function () {
+        return this.rooms - this.booked; // Need "this" (refers keyword) because inside function
     }
-  };
+};
 
 //   function checkAvailability() {
 //       return hotel.rooms - hotel.booked;
 //   }      This function works as same as the method checkAvailabilty in the hotel object !!!
-  
-  // Update the HTML
-  var elName = document.getElementById('hotelName'); // Get element
-  elName.textContent = "Hotel Name: " + hotel.name;                   // Update HTML with property of the object
-  
-  var elRooms = document.getElementById('rooms');    // Get element
+
+// Update the HTML
+var elName = document.getElementById('hotelName'); // Get element
+elName.textContent = "Hotel Name: " + hotel.name;                   // Update HTML with property of the object
+
+var elRooms = document.getElementById('rooms');    // Get element
+
+console.log(checkAvailability);
--- week3/ex3/js/object-constructor.js
+++ week3/ex3/js/object-constructor.js
@@ -6,11 +6,14 @@ hotel.rooms = 120;
 hotel.booked = 77;
 hotel.checkAvailability = function() {
   return this.rooms - this.booked;  
-};
- 
+}; 
+
+hotel.pool = false;
  
 var elName = document.getElementById('hotelName'); // Get element
 elName.textContent = 'Hotel Name: ' + hotel.name; // Update HTML with property of the object
  
 var elRooms = document.getElementById('rooms'); // Get element
 elRooms.textContent = 'Rooms Available: ' + hotel.checkAvailability(); // Update HTML with result of method
+
+console.log(checkAvailability);
