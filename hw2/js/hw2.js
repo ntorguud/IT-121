0840060d2e278bf96f397ef923b1f36bd5a4d444
Naran Torguud <ntorguud@gmail.com>
2021-04-28 05:38:57 +0000

    Edit contents

--- hw2/js/hw2.js
+++ hw2/js/hw2.js
@@ -1,11 +1,11 @@
 // instantiate and assign values to variables
-var message = prompt("Let's play the sum game.\nPlease enter your name", "");
+var message = prompt("Let's play the sum game.\nPlease enter your name");
 // var person = prompt("Please enter your name", "Stani");
-var x;
-var y;
-var total = x + y;
-var num1 = prompt("Enter first number", "");
-var num2 = prompt("Enter second number", "");
 
-document.getElementById('message').textContent = 'Hi, ' + message + '!';
-document.getElementById('math').textContent = 'The sum of ' +  x + ' + ' + y  + ' is : ' + total;
+var num1 = prompt("Enter first number");
+var num2 = prompt("Enter second number");
+var total = parseInt(num1) + parseInt(num2);
+
+
+document.getElementById('message').textContent = 'Hi, ' + message + '!' + ' The sum of ' + num1 + ' + ' + num2 + ' is : ' + total;
+// document.getElementById('message').textContent = `Hi, ${message} ! The sum of ${num1} + ${num2} is : ${total}`;
