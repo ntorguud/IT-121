9fb17c9a0e542f2fe132ee36730fa379aa4bddf5
Naran Torguud <ntorguud@gmail.com>
2021-05-03 01:52:53 +0000

    Update HW3 js page

--- HW/js/hw3.js
+++ HW/js/hw3.js
@@ -1,15 +1,35 @@
-// Create a variable called msg to hold a message that will be shown on the page
-// Find the title of the document, and put this in the msg variable
-var msg = '<p><b>page title: </b>' + document.title + '<br />';
- 
-// Find the URL of the document and add it to the msg variable
-msg += '<b>page address: </b>' + document.URL + '<br />';
- 
-// Find the date the document was last modified and add it to the msg variable
-msg += '<b>last modified: </b>' + document.lastModified + '</p>';
- 
-// Create a variable called el to hold the element whose id attribute has a value of footer
-var el = document.getElementById('tv');
- 
-// Write the message into that element
-el.innerHTML = msg;
+class TV {
+    constructor(brandName) {
+        this.brandName = brandName;
+        this.channel = 1;
+        this.volume = 25;
+    }
+
+    getBrandName() {
+        return this.brandName;
+    }
+
+    volumeUp() {
+        // this.volume = this.volume + 1;
+        // this.volume++;
+        this.volume += 1;
+    }
+
+    volumeDown() {
+        this.volume -= 1;
+    }
+
+    channelUp() {
+        this.channel += 1;
+    }
+
+    channelDown() {
+            this.channel -= 1;
+    }            
+}
+
+var appleTvRemote = new TV("Apple TV");
+
+// console.log(appleTvRemote.getBrandName())
+
+appleTvRemote.volumeDown()
