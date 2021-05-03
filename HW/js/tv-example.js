b8a3e868e552776eac28787e53a6f043428af03c
Naran Torguud <ntorguud@gmail.com>
2021-05-03 01:24:25 +0000

    Create TV-Example JS page

---
+++ HW/js/tv-example.js
@@ -0,0 +1,58 @@
+const MAX_VOLUME = 100;
+const MIN_VOLUME = 0;
+const MAX_CHANNEL = 300;
+const MIN_CHANNEL = 0; 
+
+class TV {
+    constructor(brandName) {
+        this.brandName = brandName;
+        this.channel = 1;
+        this.volume = 25;
+    }
+
+    getBrandName() {
+        return this.brandName;
+    }
+
+    volumeUp() {
+        if(this.volume <= MAX_VOLUME) {
+        // this.volume = this.volume + 1;
+        // this.volume++;
+        this.volume += 1;
+        }
+        console.log(this.volume)
+
+    }
+
+    volumeDown() {
+        if(this.volume >= 0) {
+        this.volume -= 1;
+        }
+        console.log(this.volume)
+
+    }
+
+    channelUp() {
+        if(this.channel < 26) {
+        this.channel += 1;
+        }
+        console.log(this.channel)
+
+    }
+
+    channelDown() {
+        if(this.channel > 0) {
+            this.channel -= 1;
+        }
+
+        console.log(this.channel)
+    }
+            
+}
+
+var appleTvRemote = new TV("Apple TV");
+
+// console.log(appleTvRemote.getBrandName())
+
+appleTvRemote.volumeDown()
+
