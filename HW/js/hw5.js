62c93c33f00aeb80b04d5be7adc545361a7c340e
Naran Torguud <ntorguud@gmail.com>
2021-05-05 14:31:37 -0700

    Update HW5 js page

--- HW/js/hw5.js
+++ HW/js/hw5.js
@@ -1,10 +1,19 @@
 // need variables to store average, number of input scores, score total
-var inputTotal = 0;
-var average = inputTotal / inputCount;
-var inputCount = 0;
+var inputTotal = 0, inputCount = 0, average, entry;
 
 // need prompt to allow user to enter scores
-parseInt(prompt("Enter test score\nOr enter 999 to end entries"));
 
-// need ti use an alert to display average score
+// while(entry != 999) {
+//     inputTotal += entry;
+//     inputCount ++; /* inputCount +=1; */
 
+do{
+    entry = parseInt(prompt("Enter test score\nOr enter 999 to end entries"));
+    inputTotal += entry;
+    inputCount ++;
+    average = ((inputTotal - 999) / (inputCount -1));
+}while(entry != 999);
+
+// need to use an alert to display average score
+
+alert('Average score is ' + average);
