2d7f25f7d4147cc731416649c19bda9d5b87bc8a
Naran Torguud <ntorguud@gmail.com>
2021-05-13 13:24:30 -0700

    Change documentObject to textContent

--- HW/js/hw3.js
+++ HW/js/hw3.js
@@ -4,4 +4,4 @@ var today = new Date();
 
 var stockPriceDiv = document.getElementById('stockPrice');
 
-stockPriceDiv.innerHTML = 'I have earned around $' + value + ' from AMC stocks by ' + today
+stockPriceDiv.textContent = 'I have earned around $' + value + ' from AMC stocks by ' + today
