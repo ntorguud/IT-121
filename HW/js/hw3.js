082fe3571fc3ff18396870c681c19a056a355d31
Naran Torguud <ntorguud@gmail.com>
2021-05-13 14:34:45 -0700

    Change object Model description to Smart Phones

--- HW/js/hw3.js
+++ HW/js/hw3.js
@@ -1,15 +1,18 @@
-function TV(brand, model, yrMade) {
+function SmartPhone(brand, model, yrMade) {
     this.brand = brand;
     this.model = model;
     this.yrMade = yrMade;
-    this.tvAvailable = function() {
+    this.phonesAvailable = function() {
         return '<p>Brand: ' + this.brand + ' Model: ' + this.model 
     +' Year made: ' + this.yrMade + '</p>'
     };
 }
 
-var tv1 = new TV('Samsung', 'XX1', 2018);
-var tv2 = new TV('Panasonic', 'XX2', 2019);
+var phone1 = new SmartPhone('Apple', 'iPhone12 Pro', 2020);
+var phone2 = new SmartPhone('Samsung', 'Galaxy S21', 2021);
+var phone3 = new SmartPhone('LG', 'Stylo 6', 2021);
 
-document.getElementById('tv1').innerHTML = tv1.tvAvailable();
-document.getElementById('tv2').innerHTML = tv2.tvAvailable();
+document.getElementById('phone1').innerHTML = phone1.phonesAvailable();
+document.getElementById('phone2').innerHTML = phone2.phonesAvailable();
+document.getElementById('phone3').innerHTML = phone3.phonesAvailable();
+
