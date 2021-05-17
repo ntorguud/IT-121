59ba60e612e63073dcff755eba642e2dd8476bae
Naran Torguud <ntorguud@gmail.com>
2021-05-17 11:50:10 -0700

    Add set attribute DOM

--- HW/js/hw6.js
+++ HW/js/hw6.js
@@ -38,3 +38,10 @@ if (secondItem.hasAttribute('class')) {          // If it has class attribute
   el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
  
 }
+
+var firstItem = document.getElementById('one'); // Get the first item
+firstItem.className = 'complete';               // Change its class attribute
+
+var fourthItem = document.getElementsByTagName('li').item(3);// Get fourth item
+// NOTE: The following line should say fourthItem (not el2)
+fourthItem.setAttribute('class', 'hot');       // Add an attribute to it
