02efa7052ac690b5a7e8b509c525b16afa39e3d6
Naran Torguud <ntorguud@gmail.com>
2021-05-30 08:51:30 -0700

    Update pages

---
+++ language.php
@@ -0,0 +1,88 @@
+<?php include 'includes/header.php' ?>
+
+    <div class="main">
+        <div class="splash">
+            <p>Communities near you</p>
+        </div>
+
+        <div class="fourth">
+            <h1>Popular Communities</h1>
+            <div class="popcomms">
+                <h2>Com1</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+            <div class="popcomms">
+                <h2>Com2</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+
+        <div class="fourth">
+            <h1>Popular Communities</h1>
+            <div class="popcomms">
+                <h2>Com3</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+            <div class="popcomms">
+                <h2>Com4</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+
+        <div class="fourth">
+            <h1>New Communities</h1>
+            <div class="newcomms">
+                <h2>Com5</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+            <div class="newcomms">
+                <h2>Com6</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+
+        <div class="fourth">
+            <h1>New Communities</h1>
+            <div class="newcomms">
+                <h2>Com7</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+            <div class="newcomms">
+                <h2>Com8</h2>
+                <img src="" alt="">
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+
+        <div class="categories">
+            <h1>Categories</h1>
+            <div class="cat">
+                <div class="types"><a href="hobbies.php">Hobbies</a></div>
+                <div class="types"><a href="sports.php">Sports</a></div>
+                <div class="types"><a href="food.php">Food</a></div>
+                <div class="types"><a href="language.php">Language</a></div>
+                <div class="types"><a href="event.php">Event</a></div>
+                <div class="types"><a href="interest.php">Interests</a></div>
+                <div class="types"><a href="tech.php">Tech</a></div>
+                <div class="types"><a href="support.php">Support</a></div>
+                <div class="types"><a href="conversation.php">Conversation</a></div>
+                <div class="types"><a href="practice.php">Practice</a></div>
+                <div class="types"><a href="organization.php">Organization</a></div>
+                <div class="types"><a href="communities.php">12</a></div>
+                <div class="types"><a href="communities.php">13</a></div>
+                <div class="types"><a href="communities.php">14</a></div>
+                <div class="types"><a href="communities.php">15</a></div>
+            </div> <!-- end cat-->
+        </div> <!-- end categories-->
+        <br>
+        <br>
+    </div><!-- end main-->
+
+    <?php include 'includes/footer.php' ?>
