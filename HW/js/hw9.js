5539472f163df9fe1858e77763d76751b98f1a88
Naran Torguud <ntorguud@gmail.com>
2021-06-17 14:23:02 -0700

    Add jQuery methods and effects

--- HW/js/hw9.js
+++ HW/js/hw9.js
@@ -1,80 +1,56 @@
-//random
-function rng() {
-    var random = Math.floor((Math.random() * 10) + 1);
-    if(random==10){
-      alert("Winner winner chicken dinner! You get nothing! Good Day Sir!");
-      }
-    }
-  
-  window.onload = rng();
-  //end random
-  
   $( function() {
     $( ".accordion" ).accordion();
   } );
   
-  window.ondragenter = function startTime(){
-    var today = new Date();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-  
-    document.getElementById('timer').textContent = alert('You are on this page for ' + s + ' seconds.');
-  };
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-  
-  
+
   $( function() {
-    $( "#tabsS" ).tabs();
+    $( "#tabs" ).tabs();
   } );
-  
+
+
   $( function() {
-    $( "#tabsM" ).tabs();
+    $( "#draggable" ).draggable();
   } );
-  
-  // Select the button
-  window.onload = function themeToggle() {
-    const toggleButton = document.getElementById("check-theme-toggle");
-    // Select the stylesheet <link>
-    const theme = document.getElementById("theme-link");
-    //check if there is any saved theme in the local storage
-    const savedTheme = localStorage.getItem("savedTheme");
-    const savedToggle = localStorage.getItem("savedToggle");
-  
-      if (savedTheme) {
-        theme.href = savedTheme;
+
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+
+  $( function() {
+    // run the currently selected effect
+    function runEffect() {
+      // get effect type from
+      var selectedEffect = $( "#effectTypes" ).val();
+ 
+      // Most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 50 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 280, height: 185 } };
       }
+ 
+      // Run the effect
+      $( "#effect" ).show( selectedEffect, options, 500, callback );
+    };
+ 
+    //callback function to bring a hidden box back
+    function callback() {
+      setTimeout(function() {
+        $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+      }, 1000 );
+    };
+ 
+    // Set effect from select menu value
+    $( "#button" ).on( "click", function() {
+      runEffect();
+    });
+ 
+    $( "#effect" ).hide();
+  } );
+
   
-      if (savedToggle) {
-        toggleButton.checked = savedToggle;
-      }
-  
-    // Listen for a click on the button
-    toggleButton.addEventListener("change", function(event) {
-  
-      // If the current URL contains "light-theme"
-      if (theme.getAttribute("href") == "../css/styles.css") {
-        // ... then switch it to "dark-theme"
-        theme.href = "../css/styles-dark.css";
-        localStorage.setItem("savedTheme", "../css/styles-dark.css");
-  
-        
-      // Otherwise...
-  
-      } else {
-        // ... switch it to "light-theme"
-        theme.href = "../css/styles.css";
-        localStorage.setItem("savedTheme", "../css/styles.css");
-    }
-  
-    const checkbox = event.target;  
-    localStorage.setItem("savedToggle", checkbox.checked);
-  
-  })
-  }
   
