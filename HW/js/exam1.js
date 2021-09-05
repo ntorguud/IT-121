62c4ce1b135f20b409339805d22067cd784339e8
Naran Torguud <ntorguud@gmail.com>
2021-09-04 20:48:51 -0700

    Add Tests

--- HW/js/exam1.js
+++ HW/js/exam1.js
@@ -39,3 +39,49 @@ function primeNum(num) {
     }
     return yesPrimeNum;
 }
+
+
+//Make an array with random 10 elements, then switch max and min numbers' order.
+var arr = [];
+
+for(var i = 1; i <= 10; i++) {
+    var num = Math.random() * 1000;
+    var floorNum = Math.floor(num);
+    console.log(num + '-iig toimlood ' + floorNum);
+
+    arr.push(num);
+}
+console.log(arr);
+
+var min = 100; // length
+var minIndex = -1;
+
+for(i = 0; i < arr.length; i++) {
+    console.log(arr[i]); // All elements will be printed.
+    if(arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+    }
+}
+console.log('Min element is ' + min + ', and its index is ' + minIndex);
+
+var max = 1;
+var maxIndex = -1;
+
+for(i = 0; i < arr.length; i++) {
+    console.log(arr[i]); // All elements will be printed.
+    if(arr[i] > max) {
+        max = arr[i];
+        maxIndex = i;
+    }
+}
+console.log('Max element is ' + max + ', and its index is ' + maxIndex);
+
+var turHadgalsanUtga = arr[minIndex];
+
+arr[minIndex] = arr[maxIndex];
+arr[maxIndex] = turHadgalsanUtga;
+
+for(i = 0; i < arr.length; i++) {
+    console.log(arr[i]); // All elements will be printed.
+}
