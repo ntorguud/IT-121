a2cdd35653a5668de17fb7ab7f557ac5677e4de0
Naran Torguud <ntorguud@gmail.com>
2021-11-15 15:01:08 +0800

    Reduce, Filter exercise

--- HW/jsES6/reduceFilter-Array.js
+++ HW/jsES6/reduceFilter-Array.js
@@ -27,6 +27,51 @@ arr.forEach(e => sum = sum + e);
 
 
 //5 Reduce function ashiglay. Execute hiigeed array-s gantsxan utga butsaana.
+let sum = arr.reduce((a, b) => {
+    return a + b;
+    }, 0);   //===========
+
+let sum = arr.reduce((a, b) => a + b);
+
+// function add(a, b) {
+//     return a + b;
+// }
 
 console.log(arr);
 console.log(sum);
+
+
+
+///Min numberiig oloy
+let min = arr.reduce(findMin);
+
+function findMin(min, b) {
+    if(b < min) min = b;
+
+    return min;
+}
+
+
+//In short
+let min = arr.reduce((min, b) => {
+    if(b < min) min = b;
+
+    return min;
+});
+
+
+//In very short
+let min = arr.reduce((min, b) => {return b < min ? b : min});
+
+console.log(min);
+
+
+
+////Next
+let found = arr.filter(function(e) {
+    if(e % 10 === 0) return e;
+});
+
+//In short
+let found = arr.filter((a, b) => {if(e % 10 === 0) return e
+});
