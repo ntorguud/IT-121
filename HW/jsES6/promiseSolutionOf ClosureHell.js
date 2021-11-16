ca1a684cc0ad53960b3b1529e4a96a0b7448f190
Naran Torguud <ntorguud@gmail.com>
2021-11-16 11:47:11 +0800

    Promise function exercise

---
+++ HW/jsES6/promiseSolutionOf ClosureHell.js
@@ -0,0 +1,49 @@
+
+///PROMISE!!!!! Solution of Closure Hell
+function get() {
+    return fetch(`https://www.epicgames.com/fortnite/en-US/home`) //fetch, then, catch functions promise-iig butsaadag.
+        .then(res => res.text()) //this is the web's HTML
+        .catch(err => console.log("Error", err));
+}
+
+
+
+////
+let p = new Promise((resolve, reject) => {
+    console.log(`Promise ajillaj bna.`);
+
+    //Zurag tanih
+
+    //Amjilttai bolbol resolve functioniig duudna.
+    resolve(); //=result
+
+    //Asuudal uusvel refect functioniig duudna.
+    reject(`Zurag het tom bna.`); //=reject
+});
+
+p.then(result => console.log(`Amjilttai ${result}`));
+p.catch(error => console.log(`Asuudal garlaa ${error}`));
+
+
+
+/////Tovchilvol
+new Promise((resolve, reject) => {
+    console.log(`Promise ajillaj bna.`);
+
+    //Zurag tanih function hiilee gej bodoy
+
+    //Amjilttai bolbol resolve functioniig duudna.
+    resolve(); //=result
+
+    //Asuudal uusvel refect functioniig duudna.
+    reject(`Zurag het tom bna.`); //=reject
+}).p.then(result => console.log(`Amjilttai ${result}`)).catch(error => console.log(`Asuudal garlaa ${error}`));
+
+
+
+//Golduu belen library-s function duuddag. Ex: fetch, updateOne...
+
+moveTo(50, 50)
+    .then(() => moveTo(20, 100))
+    .then(() => moveTo(100, 200))
+    .then(() => moveTo(2, 10))
